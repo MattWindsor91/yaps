3bc14ccdba9565ed42044996901cc56bd72b9fd3
mattbw <matt.windsor@ury.york.ac.uk>
2016-11-29 19:51:24 +0000

    Add more getters etc.

--- list/list.go
+++ list/list.go
@@ -30,24 +30,32 @@ func (a AutoMode) String() string {
 	panic(fmt.Sprintf("unknown automode: %#v", a))
 }
 
+// Item is the internal representation of a baps3d list item.
+type Item struct {
+	// hash is the inserter-supplied unique hash of the item.
+	hash string
+}
+
 // List is the internal representation of a baps3d list.
 // It only maintains the playlist itself: it does not talk to the environment,
 // nor does it know anything about what is actually playing.
 type List struct {
-	/* list is the internal linked list representing the playlist. */
+	// list is the internal linked list representing the playlist.
+	// Element type is *Item.
 	list *list.List
 
-	/* selection is the currently selected index, or -1 if there isn't one. */
+	// selection is the currently selected index, or -1 if there isn't one.
 	selection int
 
-	/* autoselect is the current autoselection mode. */
+	// autoselect is the current autoselection mode.
 	autoselect AutoMode
-	/* usedHashes is the set of currently spent hashes since the last select.
-	   It is used for calculating the next track in AutoShuffle mode. */
+	// usedHashes is the set of currently spent hashes since the last select.
+	// It is used for calculating the next track in AutoShuffle mode.
 	usedHashes map[string]struct{}
 }
 
 // New creates a new baps3d list.
+// The list begins with no selection, an empty list, and autoselect off.
 func New() *List {
 	return &List{
 		list:       list.New(),
@@ -57,15 +65,51 @@ func New() *List {
 	}
 }
 
+
+// Count gets the number of items in the list.
+func (l *List) Count() int {
+	return l.list.Len()
+}
+
+
+// AutoMode gets the current autoselect mode for the given List.
+func (l *List) AutoMode() AutoMode {
+	return l.autoselect
+}
+
 // SetAutoMode changes the current autoselect mode for the given List.
 func (l *List) SetAutoMode(mode AutoMode) {
 	l.autoselect = mode
 	l.clearUsedHashes()
 }
 
-// AutoMode gets the current autoselect mode for the given List.
-func (l *List) GetAutoMode() AutoMode {
-	return l.autoselect
+
+// Selection gets the current selection for the given List.
+// The selection is returned as a pair of index and possible item.
+// If the index is -1, there is no selection, and the item is nil.
+func (l *List) Selection() (int, *Item) {
+	if l.selection < -1 {
+		panic("Selection(): selection negative but not -1")
+	}	
+
+	// No selection?
+	if l.selection == -1 {
+		return -1, nil
+	}
+
+	// selection is positive, so we need to walk through the list to find it.
+	e := l.list.Front();
+	for sel := l.selection; 0 < sel; sel-- {
+		// The selection being above the number of items is an internal error.
+		if e == nil {
+			panic("Selection(): selection out of bounds")
+		}
+
+		e = e.Next()
+	}
+
+	// In this case, we've found our selected item.
+	return l.selection, e.Value.(*Item)
 }
 
 // Next advances the selection according to the automode.
--- list/list_test.go
+++ list/list_test.go
@@ -6,12 +6,31 @@ import (
 	"github.com/UniversityRadioYork/baps3d/list"
 )
 
+
+func ExampleNew() {
+	l := list.New()
+	
+	fmt.Println(l.Count())
+
+	idx, _ := l.Selection()
+	fmt.Println(idx)
+
+	fmt.Println(l.AutoMode())
+
+	// Output:
+	// 0
+	// -1
+	// off
+}
+
+
 func ExampleList_SetAutoMode() {
 	l := list.New()
-	fmt.Println(l.GetAutoMode().String())
+	fmt.Println(l.AutoMode())
 
 	l.SetAutoMode(list.AutoShuffle)
-	fmt.Println(l.GetAutoMode().String())
+	fmt.Println(l.AutoMode())
+
 	// Output:
 	// off
 	// shuffle
