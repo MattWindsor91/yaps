8d10f37930e615b765f78c93a32ed39280a91021
mattbw <matt.windsor@ury.york.ac.uk>
2017-04-27 20:49:28 +0100

    Add item constructors
    These need tests.

--- list/list.go
+++ list/list.go
@@ -2,7 +2,6 @@ package list
 
 import (
 	"container/list"
-	"fmt"
 	"time"
 	"math/rand"	
 )
@@ -28,14 +27,52 @@ func (a AutoMode) String() string {
 		return "next"
 	case AutoShuffle:
 		return "shuffle"
+	default:
+		return "?unknown?"
 	}
-	panic(fmt.Sprintf("unknown automode: %#v", a))
+}
+
+// ItemType is the type of types of item.
+type ItemType int
+
+const (
+	ItemNone ItemType = iota
+	ItemTrack
+	ItemText
+)
+
+// String gets the descriptive name of an ItemType as a string.
+func (i ItemType) String() string {
+	switch i {
+	case ItemNone:
+		return "none"
+	case ItemTrack:
+		return "track"
+	case ItemText:
+		return "text"
+	default:
+		return "?unknown?"
+	}
 }
 
 // Item is the internal representation of a baps3d list item.
 type Item struct {
 	// hash is the inserter-supplied unique hash of the item.
 	hash string
+	// payload is the data component of the item.
+	payload string
+	// itype is the type of tie item.
+	itype ItemType
+}
+
+// NewTrack creates a new track-type item.
+func NewTrack(hash, path string) *Item {
+	return &Item { hash, path, ItemTrack }
+}
+
+// NewText creates a new text-type item.
+func NewText(hash, contents string) *Item {
+	return &Item { hash, contents, ItemText }
 }
 
 // List is the internal representation of a baps3d list.
