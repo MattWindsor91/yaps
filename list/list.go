98f08d1f0bddcbd5edf47d872d170cb2cd4fcf63
mattbw <matt.windsor@ury.york.ac.uk>
2017-04-27 20:51:05 +0100

    Move Item to new file

--- list/list.go
+++ list/list.go
@@ -32,49 +32,6 @@ func (a AutoMode) String() string {
 	}
 }
 
-// ItemType is the type of types of item.
-type ItemType int
-
-const (
-	ItemNone ItemType = iota
-	ItemTrack
-	ItemText
-)
-
-// String gets the descriptive name of an ItemType as a string.
-func (i ItemType) String() string {
-	switch i {
-	case ItemNone:
-		return "none"
-	case ItemTrack:
-		return "track"
-	case ItemText:
-		return "text"
-	default:
-		return "?unknown?"
-	}
-}
-
-// Item is the internal representation of a baps3d list item.
-type Item struct {
-	// hash is the inserter-supplied unique hash of the item.
-	hash string
-	// payload is the data component of the item.
-	payload string
-	// itype is the type of tie item.
-	itype ItemType
-}
-
-// NewTrack creates a new track-type item.
-func NewTrack(hash, path string) *Item {
-	return &Item { hash, path, ItemTrack }
-}
-
-// NewText creates a new text-type item.
-func NewText(hash, contents string) *Item {
-	return &Item { hash, contents, ItemText }
-}
-
 // List is the internal representation of a baps3d list.
 // It only maintains the playlist itself: it does not talk to the environment,
 // nor does it know anything about what is actually playing.
