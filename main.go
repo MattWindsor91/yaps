439a371dfa5eb285441b2cac08e05624d3db0f54
mattbw <matt.windsor@ury.york.ac.uk>
2018-01-09 22:32:11 +0000

    Add initial client copying

--- main.go
+++ main.go
@@ -9,11 +9,36 @@ import (
 	"github.com/UniversityRadioYork/baps3d/list"
 )
 
+func copyClient(cli *comm.Client) *comm.Client {
+	sdreply := make(chan comm.Response)
+	cli.Tx <- comm.Request{
+		Origin: comm.RequestOrigin{
+			Tag:     bifrost.TagUnknown,
+			ReplyTx: sdreply,
+		},
+		Body: comm.NewClientRequest{},
+	}
+	var ncli *comm.Client
+	for {
+		r := <-sdreply
+		switch b := r.Body.(type) {
+		case comm.NewClientResponse:
+			ncli = b.Client
+		case comm.AckResponse:
+			return ncli
+		}
+	}
+}
+
 func main() {
 	lc, cli := list.NewControlledList()
 	go lc.Run()
 
+	tcli := copyClient(cli)
+	
 	lb, ltx, lrx := list.NewBifrost(cli)
+	lb.Fork(tcli)
+	
 	go lb.Run()
 	console, err := console.New(ltx, lrx)
 	if err != nil {
