7e3c4dda1f6c2e1f1cf94bfd55889e1356e7204f
mattbw <matt.windsor@ury.york.ac.uk>
2018-04-01 14:16:35 +0100

    More graceful shutdown cruft

--- netsrv/server.go
+++ netsrv/server.go
@@ -3,6 +3,7 @@ package netsrv
 import (
 	"log"
 	"net"
+	"sync"
 
 	"github.com/UniversityRadioYork/baps3d/comm"
 )
@@ -25,6 +26,10 @@ type Server struct {
 
 	// clients is a map containing all connected clients.
 	clients map[client]struct{}
+
+	// wg is a WaitGroup that tracks all inner server goroutines.
+	// The server main loop won't terminate until the WaitGroup hits zero.
+	wg sync.WaitGroup
 }
 
 // client holds the server-side state of a baps3d TCP client.
@@ -77,12 +82,14 @@ func (s *Server) hangUpAllClients() {
 
 // hangUpClient closes the client pointed to by c.
 func (s *Server) hangUpClient(c *client) {
+	s.l.Println("hanging up:", c.conn.RemoteAddr().String())
 	c.Close()
 	delete(s.clients, *c)
 }
 
 // Run runs the net server main loop.
 func (s *Server) Run() {
+	defer s.wg.Wait()
 	defer s.shutdownController()
 
 	ln, err := net.Listen("tcp", s.host)
@@ -91,13 +98,29 @@ func (s *Server) Run() {
 		return
 	}
 
-	defer ln.Close()
-	s.l.Println("now listening on", s.host)
-
 	connCh := make(chan net.Conn)
 	cerrCh := make(chan error)
 
-	go acceptClients(ln, connCh, cerrCh)
+	defer func() {
+		s.hangUpAllClients()
+		if err := ln.Close(); err != nil {
+			s.l.Println("error closing listener:", err)
+		}
+		s.l.Println("closed listener")
+
+		// The acceptor is going to send us a 'my listener has closed!'
+		// error, then close the channel.  This means we'll have to
+		// pretend to listen for that error first.
+		for _ = range cerrCh {
+		}
+	}()
+
+	s.l.Println("now listening on", s.host)
+	s.wg.Add(1)
+	go func() {
+		s.acceptClients(ln, connCh, cerrCh)
+		s.wg.Done()
+	}()
 
 	for {
 		select {
@@ -108,23 +131,29 @@ func (s *Server) Run() {
 			s.newClient(conn)
 		case <-s.rootClient.Done:
 			s.l.Println("received controller shutdown")
-			s.hangUpAllClients()
 			return
-
 		}
 	}
 }
 
 // acceptClients keeps spinning, accepting clients on ln and sending them to
 // connCh, until ln closes.
-// It then sends the error on errCh.
-func acceptClients(ln net.Listener, connCh chan<- net.Conn, cerrCh chan<- error) {
+// It then sends the error on errCh and closes both channels.
+func (s *Server) acceptClients(ln net.Listener, connCh chan<- net.Conn, cerrCh chan<- error) {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			cerrCh <- err
+			close(cerrCh)
 			return
 		}
-		connCh <- conn
+		select {
+		case connCh <- conn:
+		case <-s.rootClient.Done:
+			// The main loop will have closed, so won't be listening
+			// for connections, but we're waiting for it to close our acceptor.
+			// TODO(@MattWindsor91): necessary?
+			conn.Close()
+		}
 	}
 }
